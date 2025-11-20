cf48d7192ab8365ac4f97ef358ad90f459b24ca0
TonyScaraboto <antonyitallo@gmail.com>
2025-11-20 18:21:46 -0300

    Delete acessorio.sql

--- acessorio.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS acessorios (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nome TEXT NOT NULL,
-    quantidade INTEGER NOT NULL,
-    preco_unitario REAL NOT NULL,
-    receita_total REAL NOT NULL,
-    data_venda TEXT NOT NULL
-);
